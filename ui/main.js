8b5985092b073883dd29e23d3fa4709bc6ce0424
shafiulla171 <shfa14ec@cmrit.ac.in>
2017-02-09 18:22:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 //counter code
 var button=document.getElementById('counter');
 var counter=0;
-button.onclick=function(){
+button.onclick = function () {
     //to render the variable in correct span
-     counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+     counter=counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
