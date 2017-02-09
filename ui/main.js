1f937d7d67d8c797a4f8fe77164eff53ecf1bc13
shafiulla171 <shfa14ec@cmrit.ac.in>
2017-02-09 18:20:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
     //to render the variable in correct span
+     counter=counter+1;
     var span=document.getElementById('count');
-    counter=counter+1;
     span.innerHTML=counter.toString();
 };
