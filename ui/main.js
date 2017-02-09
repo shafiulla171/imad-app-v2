fa0513974f611adceedd2011abdae47af04a1c10
shafiulla171 <shfa14ec@cmrit.ac.in>
2017-02-09 13:53:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,18 @@
 console.log('Loaded!');
 var img=document.getElementById("madi");
 img.onclick = function(){
-   var interval = setInterval(moveleft,500);
+   var interval = setInterval(moveleft,5);
 };
 var Right=0;
 function moveleft() {
-    Right = Right+1000;
-    img.style.marginRight=Right ;
-    
-}
+    Right = Right+10;
+    img.style.marginRight=Right +'px';
+};
+img.ondblclick = function(){
+    var interval = setInterval(moveright,50);
+};
+var Right=0;
+function moveright() {
+    Right = Right+100;
+    img.style.marginleft=Right +'px';
+};
