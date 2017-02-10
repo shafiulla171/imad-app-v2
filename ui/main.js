56487653c0b1eaf5e030344bae56cdb1464f2274
shafiulla171 <shfa14ec@cmrit.ac.in>
2017-02-10 18:50:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,10 @@
 //counter code
 var button=document.getElementById('counter');
-button.onclick = function () {
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_bt');
+submit.onclick = function() {
     //craete request to counter end point
     var request = new XMLHttpRequest();
     //capture the response and store it
@@ -8,33 +12,25 @@ button.onclick = function () {
         if(request.readyState == XMLHttpRequest.DONE){
             //take some action
             if(request.status == 200){
-             var counter = request.responseText;
-             var span = document.getElementById('count');
-             span.innerHTML = counter.toString();
-            
+                 //capture the name
+                  var names = ['name1','name2','name3','name4'];
+                  var list='';
+                  for (var i=0; i<names.length; i++){
+                        list += '<li>'+ names[i] +'</li>';
+        
+                         }
+                  var ul = document.getElementById('namelist');
+                   ul.innerHTML= list;
+    
+               }
         }
-    }
     };
    //to make request
-   request.open('GET','http://shafiulla171.imad.hasura-app.io/counter',true);
+   request.open('GET','http://shafiulla171.imad.hasura-app.io/submit-name/name' + name,true);
    request.send(null);
     
 };
-//submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
-var submit = document.getElementById('submit_bt');
-submit.onclick = function() {
+
     //create a request to server
     
-    //capture the name
-    var names = ['name1','name2','name3','name4'];
-    var list='';
-    for (var i=0; i<names.length; i++){
-        list += '<li>'+ names[i] +'</li>';
-        
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML= list;
-    
-}
+   
