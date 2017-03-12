543a06be76b945a126b1eb85a4ecdccdcc7dad75
shafiulla171 <shfa14ec@cmrit.ac.in>
2017-03-12 11:29:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,13 +5,97 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles={
+     'article-one':{
+    tittle:'Article One | shafeeq',
+    heading:'this is article one',
+    date:'8 feb 2017',
+    content:` <p>
+                  this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+              </p>
+              <p>
+                this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+            </p>
+            <p>
+            this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+            </p>`
+     },
+     'article-two':{
+    tittle:'Article Two | shafeeq',
+    heading:'this is article Two',
+    date:'8 feb 2017',
+    content:` <p>
+                  this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.this is the content for my first article.
+              </p>
+          `
+     },
+     'article-three':{
+         tittle:'Article Two | shafeeq',
+    heading:'this is article Two',
+    date:'8 feb 2017',
+    content:'<p>this is just to see</P>'
+    }
+};
+function createTemplate(data){
+var tittle=data.tittle;
+var date=data.date;
+var content=data.content;
+var heading=data.heading;
+var htmlTemplate=
+` <html>
+    <head>
+        <title>
+           ${tittle}
+        </title>
+      <link href="/ui/style.css" rel="stylesheet" />
+    
+     </head>
+    <body>
+        <div class='container'>
+        <div> 
+        <a href='/'>home</a>
+        </div>
+        <hr>
+        <h3>
+          ${heading}
+        </h3>
+        <div>
+           ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>`;
+return htmlTemplate;
+}
 
+var counter=0;
+app.get('/counter',function(req,res){
+  counter = counter+1;
+  res.send(counter.toString());
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var names = [];
+app.get('/submit-name',function(req,res) {//querry submit-name?name=xx
+    var name=req.query.name;
+    //pushing this typed name into names
+    names.push(name); 
+    //javascript object notation
+    res.send(JSON.stringify(names));
+});
 
+app.get('/:articleName',function(req,res){
+    //articleName=article-one
+    //articles[articleName]=contents of article-one
+    var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -21,10 +105,14 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
