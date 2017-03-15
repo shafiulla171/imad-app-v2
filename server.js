f0007f691c5a143d459db8f433d429f2d336df16
shafiulla171 <shfa14ec@cmrit.ac.in>
2017-03-15 19:34:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/article/:articleName',function(req,res){
       if(err){
           res.status.send(err.toString());
       }else{
-          if(result.rows.length==0){
+          if(result.rows.length===0){
               res.status(404).send('article not found');
           }else{
              var  articleData=result.rows;
